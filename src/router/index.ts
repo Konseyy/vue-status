6e959e1b2636dbfe195fc7d71e343285d27355dd
Konsey <vgbukalders@gmail.com>
2022-04-05 22:57:29 +0300

    possible 404 fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import ProjectView from '@/views/ProjectView.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //   history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
@@ -16,12 +17,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TaskView.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: ProjectView,
-    },
+    }
   ],
 });
 
